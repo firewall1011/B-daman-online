88bbb0cdc3eea5c436e34bc88b845a5d72bd2325
Tiago Marino <tmsilva11@gmail.com>
2021-02-06 01:12:07 -0300

    Add Player Camera

---
+++ Assets/CameraActivateOnStartAutority.cs
@@ -0,0 +1,14 @@
+﻿using Mirror;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraActivateOnStartAutority : NetworkBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera _virtualCamera = default;
+
+    public override void OnStartAuthority()
+    {
+        _virtualCamera.gameObject.SetActive(true);
+    }
+}
